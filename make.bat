4ea8cd15058bca4306fbcf2ee8a9bcc8803698e3
paaco <paaco@users.noreply.github.com>
2022-04-20 10:11:16 +0200

    Fix ALZ RAM/ROM settings

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 acme -f cbm -DDEBUG=0 -o .cache/hardfoam-release.prg hardfoam.asm
 exomizer sfx sys .cache/hardfoam-release.prg -o hardfoam.exo.prg
-alz64 -s -a .cache\hardfoam-release.prg hardfoam.alz.prg
+alz64 -s -1 $37 .cache\hardfoam-release.prg hardfoam.alz.prg
 dir hardfoam*.prg
